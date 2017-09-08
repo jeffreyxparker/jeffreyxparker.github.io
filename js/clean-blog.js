83712e86dc535c68b772050db4fa6ef47264fac6
Jeff Parker <jeffreyparker2017@u.northwestern.edu>
2017-09-07 21:06:10 -0500

    Removing contact form, rolling back comments, favicon cleanup

--- js/clean-blog.js
+++ js/clean-blog.js
@@ -9,42 +9,6 @@ $(function() {
     $("[data-toggle='tooltip']").tooltip();
 });
 
-// Contact Form Script
-$(function() {
-    $("#contactForm").submit(function(e) {
-      e.preventDefault();
-      $.ajax({
-        url: "//formspree.io/jeffreyxparker@gmail.com", 
-        method: "POST",
-        data: $(this).serialize(),
-        dataType: "json",
-        success: function(data){
-          // Success message
-          $('#success').html("<div class='alert alert-success'>");
-          $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
-              .append("</button>");
-          $('#success > .alert-success')
-              .append("<strong>Your message has been sent. </strong>");
-          $('#success > .alert-success')
-              .append('</div>');
-
-          //clear all fields
-          $('#contactForm').trigger("reset");
-        },
-        error: function(){
-          // Fail message
-          $('#success').html("<div class='alert alert-danger'>");
-          $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
-              .append("</button>");
-          $('#success > .alert-danger').append("<strong>Sorry it seems that my mail server is not responding. Please try again later!");
-          $('#success > .alert-danger').append('</div>');
-          //clear all fields
-          $('#contactForm').trigger("reset");
-        }
-      });
-    });
-});
-
 /*When clicking on Full hide fail/success boxes */
 $('#name').focus(function() {
     $('#success').html('');
